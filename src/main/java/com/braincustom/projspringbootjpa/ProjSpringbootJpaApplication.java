577ab3250d67756e553a92945b9c8187c23746e6
hvalmer <hvalmer@gmail.com>
2020-05-02 16:45:52 -0300

    Project created

---
+++ src/main/java/com/braincustom/projspringbootjpa/ProjSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.braincustom.projspringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjSpringbootJpaApplication.class, args);
+	}
+
+}
