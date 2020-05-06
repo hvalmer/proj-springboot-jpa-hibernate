6693c57995e09a87b3f15c3e17bc842149be1421
hvalmer <hvalmer@gmail.com>
2020-05-06 13:18:06 -0300

    OrderItem, many-to-many association with extra attributes - Part 2

--- src/main/java/com/braincustom/projspringbootjpa/config/TestConfig.java
+++ src/main/java/com/braincustom/projspringbootjpa/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.braincustom.projspringbootjpa.entities.Category;
 import com.braincustom.projspringbootjpa.entities.Order;
+import com.braincustom.projspringbootjpa.entities.OrderItem;
 import com.braincustom.projspringbootjpa.entities.Product;
 import com.braincustom.projspringbootjpa.entities.User;
 import com.braincustom.projspringbootjpa.entities.enums.OrderStatus;
 import com.braincustom.projspringbootjpa.repositories.CategoryRepository;
+import com.braincustom.projspringbootjpa.repositories.OrderItemRepository;
 import com.braincustom.projspringbootjpa.repositories.OrderRepository;
 import com.braincustom.projspringbootjpa.repositories.ProductRepository;
 import com.braincustom.projspringbootjpa.repositories.UserRepository;
@@ -34,6 +36,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -74,11 +79,17 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
-		//salvando os usuarios no BD
+		//salvando os pedidos no BD
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
+		//salvando os pedidos no BD
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 		
 	}
