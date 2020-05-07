69006ad271011fe587d8be4a683149d0ea0a8048
hvalmer <hvalmer@gmail.com>
2020-05-07 19:53:54 -0300

    User update

--- src/main/java/com/braincustom/projspringbootjpa/services/UserService.java
+++ src/main/java/com/braincustom/projspringbootjpa/services/UserService.java
@@ -35,4 +35,17 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	//atualizando um usuário no BD
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
